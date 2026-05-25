e9ca6fdb6f135ce8e486fb398f4852b1cc0757da
lucaspereirasouza <lucaspereiradesouza2163@outlook.com>
2026-05-25 17:55:47 -0300

    feat: refactor screenshare implementation to use WebRTC via aiortc and mss

--- screenshare.py
+++ screenshare.py
@@ -1,22 +1,31 @@
-#Copyright (C) 2021  Qijun Gu
-#
-#This file is part of Screenshare.
-#
-#Screenshare is free software: you can redistribute it and/or modify
-#it under the terms of the GNU General Public License as published by
-#the Free Software Foundation, either version 3 of the License, or
-#(at your option) any later version.
-#
-#Screenshare is distributed in the hope that it will be useful,
-#but WITHOUT ANY WARRANTY; without even the implied warranty of
-#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#GNU General Public License for more details.
-#
-#You should have received a copy of the GNU General Public License
-#along with Screenshare. If not, see <https://www.gnu.org/licenses/>.
+# Copyright (C) 2026
+# WebRTC-based Screenshare
 
-from flask import Flask, request, flash, session
+import argparse
+import asyncio
+import json
+import threading
+#import concurrent.futures
+import time
+from fractions import Fraction
+
+import traceback
+
+from av.frame import Frame
+
+from flask import Flask, request, render_template_string
 from flask_bootstrap import Bootstrap
+from aiortc import (
+    RTCPeerConnection,
+    RTCSessionDescription,
+    MediaStreamTrack
+)
+from av import VideoFrame
+
+import mss
+import numpy as np
+
+
 HTML_PAGE = """
 <!DOCTYPE html>
 <html>
@@ -79,68 +88,178 @@ start()
 """
 
 app = Flask(__name__)
-app.secret_key = secret_key
-app.config['BOOTSTRAP_SERVE_LOCAL'] = True
 Bootstrap(app)
 
+pcs = set()
 
-###### general ##########################################
+class MediaStreamError(Exception):
+    pass
+
+class ScreenTrack(MediaStreamTrack):
+    kind = "video"
+    VIDEO_PTIME = 0.02
+    VIDEO_CLOCK_RATE = 90000
+    VIDEO_TIME_BASE = Fraction(1, VIDEO_CLOCK_RATE)
+    def __init__(self, fps=30, with_cursor=True):
+        super().__init__()
+
+        self.fps = fps
+
+        # Initialize mss for screen capture
+        self.sct = mss.mss(with_cursor=with_cursor)
+        self.monitor = self.sct.monitors[0]
+
+
+    async def next_timestamp(self):
+       if self.readyState != "live":
+           print("live stream error")
+           raise MediaStreamError
+       if hasattr(self, "_timestamp"):
+           self._timestamp += int(self.VIDEO_PTIME * self.VIDEO_CLOCK_RATE)
+           wait = self._start + (self._timestamp / self.VIDEO_CLOCK_RATE) - time.time()
+           await asyncio.sleep(wait)
+       else:
+           self._start = time.time()
+           self._timestamp = 0
+       return self._timestamp, self.VIDEO_TIME_BASE
+
+    async def recv(self):
+        pts, time_base = await self.next_timestamp()
+
+        # Grab the screen (this is a fast synchronous call)
+        sct_img = self.sct.grab(self.monitor)
+        
+        # Convert the mss screenshot to a numpy array (it defaults to BGRA)
+        img_np = np.array(sct_img)
+        
+        # Strip the alpha channel to convert BGRA to BGR
+        img_bgr = img_np[:, :, :3]
+
+        frame = VideoFrame.from_ndarray(
+            img_bgr,
+            format="bgr24"
+        )
+
+        frame.pts = pts
+        frame.time_base = time_base
+        return frame
+
+loop = asyncio.new_event_loop()
+
+def start_loop():
+    asyncio.set_event_loop(loop)
+    loop.run_forever()
+
+threading.Thread(
+    target=start_loop,
+    daemon=True
+).start()
+
+
+
 @app.route('/')
-def welcome():
-    session.clear()
-    if len(screenlive.password) == 0 :
-        session['access'] = True
-        return render_template("screen.html")
-    else :
-        return render_template("login.html")
+def index():
+    return render_template_string(HTML_PAGE)
 
-@app.route('/login', methods = ['POST'])
-def login():
-    # password is not required
-    session.clear()
-    if len(screenlive.password) == 0 :
-        session['access'] = True
-        return render_template("screen.html")
 
-    p = request.form["password"]
-    if p == screenlive.password :
-        session['access'] = True
-        return render_template("screen.html")
-    else :
-        session.clear()
-        flash("Wrong password")
-        return render_template("login.html")
+@app.route('/offer', methods=['POST'])
+def offer():
+    params = request.json
+    future = asyncio.run_coroutine_threadsafe(
+        handle_offer(params),
+        loop
+    )
+    result = future.result()
+    traceback.print_exc
+    return result
 
-@app.route('/screenfeed/', methods=["POST"])
-def screenfeed():
-    if 'access' in session and session['access']:
-        return json.dumps([True, screenlive.gen()])
-    else:
-        redirect('/')
+  
 
-### main ###
+async def handle_offer(params):
+    offer = RTCSessionDescription(
+        sdp=params['sdp'],
+        type=params['type']
+    )
+
+    pc = RTCPeerConnection()
+
+    pcs.add(pc)
+
+    @pc.on("connectionstatechange")
+    async def on_connectionstatechange():
+        print("Connection state:", pc.connectionState)
+
+        if pc.connectionState in ["failed", "closed"]:
+            await pc.close()
+            pcs.discard(pc)
+
+    track = ScreenTrack(fps=30)
+    pc.addTrack(track)
+
+    await pc.setRemoteDescription(offer)
+
+    answer = await pc.createAnswer()
+
+    await pc.setLocalDescription(answer)
+
+    return json.dumps({
+        'sdp': pc.localDescription.sdp,
+        'type': pc.localDescription.type
+    })
+
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument("-p", "--port", help="port, default 8888", type=int, default=8888)
-    parser.add_argument("-w", "--password", help="password, default no password", default="")
-    parser.add_argument("-s", "--https", help="enable https, default http", action="store_true")
-    parser.add_argument("-c", "--cert", help="certificate file")
-    parser.add_argument("-k", "--key", help="private key file")
 
-    parser.print_help()
+    parser.add_argument(
+        '-p',
+        '--port',
+        help='port, default 8888',
+        type=int,
+        default=8888
+    )
+
+    parser.add_argument(
+        '-s',
+        '--https',
+        help='enable https',
+        action='store_true'
+    )
+
+    parser.add_argument(
+        '-c',
+        '--cert',
+        help='certificate file'
+    )
+
+    parser.add_argument(
+        '-k',
+        '--key',
+        help='private key file'
+    )
+
     args = parser.parse_args()
-    port = args.port
-    screenlive.password = args.password
-    
-    try:
-        if args.https:
-            if args.cert and args.key:
-                app.run(host='0.0.0.0', port=port, threaded=True, ssl_context=(args.cert, args.key))
-            else:
-                app.run(host='0.0.0.0', port=port, threaded=True, ssl_context='adhoc')
+
+    print(f"Starting WebRTC screenshare on port {args.port}")
+
+    if args.https:
+        if args.cert and args.key:
+            app.run(
+                host='0.0.0.0',
+                port=args.port,
+                threaded=True,
+                ssl_context=(args.cert, args.key)
+            )
         else:
-            app.run(host='0.0.0.0', port=port, threaded=True)
-    except Exception as e:
-        print(e.message)
-        print("Some errors in the command, fall back to the default http screen sharing!!!\n")
-        app.run(host='0.0.0.0', port=port, threaded=True)
+            app.run(
+                host='0.0.0.0',
+                port=args.port,
+                threaded=True,
+                ssl_context='adhoc'
+            )
+    else:
+        app.run(
+            host='0.0.0.0',
+            port=args.port,
+            threaded=True
+        )
